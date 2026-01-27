37b4c6050abc961d859bd414d83c4875f4530833
Dev-Mjarir08 <08mjarir@gmail.com>
2026-01-27 12:00:00 +0530

    db-setup

--- day-7-setup-mdb/index.js
+++ day-7-setup-mdb/index.js
@@ -2,7 +2,8 @@ import express from 'express';
 import { env } from './config/dotenv.js';
 import db from './config/db.js';
 import bodyParser from 'body-parser';
-import { createUser } from './controllers/user.controller.js';
+import UserModel from './models/user.model.js';
+
 
 const port = env.PORT || 3000;
 const app = express();
@@ -14,8 +15,43 @@ app.get('/', (req, res) => {
   return res.render('index');
 });
 
-// 👇 INSERT INTO DB
-app.post('/user/create', createUser);
+app.post('/', (req, res) => {
+  UserModel.create(req.body)
+    .then(() => {
+      res.redirect('/');
+    })
+    .catch((err) => {
+      console.log(err);
+      res.redirect('/');
+    })
+})
+
+app.get('/viewData', (req, res) => {
+  UserModel.find()
+    .then((users) => {
+      res.render('pages/viewData', { users });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.render('/', {
+        users: []
+      });
+    });
+});
+
+app.get('/delete/:id', (req, res) => {
+  const id = req.params.id;
+  UserModel.findByIdAndDelete(id)
+    .then(() => {
+      res.redirect('/viewData');
+    })
+    .catch((err) => {
+      console.log(err);
+      res.redirect('/viewData')
+    })
+})
+
+
 
 app.listen(port, () => {
   console.log('Server Started');
